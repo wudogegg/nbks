1ed06affea04e46b9d4cb4c21f8fe38d0b4476c9
Your Name <951437256@.com>
2020-06-17 09:52:13 +0800

    第二次提交

--- src/main/java/com/cn/Person.java
+++ src/main/java/com/cn/Person.java
@@ -4,4 +4,11 @@ public class Person {
     private  Integer age;
     private String name;
     private  String sex;
+
+    private  Integer age1;
+    private String name1;
+    private  String sex2;
+    private  Integer age2;
+    private String name3;
+    private  String sex3;
 }
