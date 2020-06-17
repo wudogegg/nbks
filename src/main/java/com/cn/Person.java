23e1b2b040ba254f7b1a4bfae3987f3187ebf94e
wudogegg <66986019+wudogegg@users.noreply.github.com>
2020-06-17 09:57:05 +0800

    Update Person.java

--- src/main/java/com/cn/Person.java
+++ src/main/java/com/cn/Person.java
@@ -11,4 +11,12 @@ public class Person {
     private  Integer age2;
     private String name3;
     private  String sex3;
+    
+    private  Integer age6;
+    private String name6;
+    private  String sex4;
+    private  Integer age4;
+    private String name4;
+    private  String sex7;
+    
 }
